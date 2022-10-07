8045540c424a202a153d18a21491922bd6c7310f
pinnnguyen <112460736+phonggnguyen@users.noreply.github.com>
2022-10-07 08:54:55 +0700

    find colors

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,69 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import { getColors } from './utils';
+import Color from './components/color.vue';
+
+const color = ref('#1f2937')
+const colorResult = ref({})
+const messError = ref<string>('')
+
+const find = () => {
+  try {
+    colorResult.value = getColors(color.value)
+    console.log("colorResult", colorResult)
+    messError.value = ''
+  } catch (e: string) {
+    messError.value = e
+  }
+}
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div class="section">
+      <input type="text" v-model="color">
+      <button @click="find">
+      Find Colors
+    </button>
+    </div>
+    <div class="colors">
+      <Color v-for="(color, key) in colorResult"  :color="color" :keyc="key" />
+    </div>
+    <p v-if="messError">
+      {{ messError }}
+    </p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+  .section {
+    display: flex;
+    margin-bottom: 10px;
+  }
+
+  p {
+    margin: 0;
+  }
+
+  button {
+    height: 34px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  input {
+      margin-right: 10px;
+  }
+
+  .colors {
+    display: flex;
+  }
+
+  .color {
+    width: 50px;
+    height: 34px;
+    margin-right: 10px;
+    cursor: pointer;
+  }
 </style>
---
+++ src/components/color.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { useClipboard } from '@vueuse/core'
+
+defineProps({
+    color: String,
+    keyc: String
+})
+
+const source = ref('')
+const { text, copy, copied, isSupported } = useClipboard({ source })
+    
+</script>
+
+<template>
+    <div>
+        <div ref="source" class="color" :style="{'background-color': color}" @click='copy()' />
+        <p>
+            {{ keyc }}
+        </p>
+        <p style="font-size: 12px">
+            {{ color }}
+        </p>
+
+        <span v-if="copied">Copied!</span>
+    </div>
+</template>
